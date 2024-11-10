bce0aed068cc4917b11d95b6c2613c4b7571552a
tauheed0110 <tauheed0110@gmail.com>
2024-11-10 09:06:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+const name = document.getElementById("name");
+const age = document.getElementById("age");
+
+const data = {
+    name: null,
+    age: null
+}
+
+function validate() {
+    if (!name.value || !age.value) {
+        alert("inputs can't be empty.")
+        return false;
+    }
+    return true;
+}
+
+function setData() {
+    if (validate()) {
+        data.name = name.value;
+        data.age = age.value;
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (data.age >= 18) {
+                    resolve({ status: "success", data: data })
+                } else if (data.age < 18) {
+                    reject({ satus: "failed", data: data })
+                }
+            }, 4000);
+        })
+    }
+
+}
+
+window.onload = () => {
+    document.getElementById("myForm").onsubmit = (e) => {
+        e.preventDefault();
+        const promise = setData();
+        promise.then((data) => {
+            alert(`Welcome, ${data.data.name}. You can vote.`)
+        }).catch((err) => {
+            alert(`Oh sorry ${err.data.name}. You aren't old enough.`)
+        })
+    }
+}
